76693541e13a41fa8a938679eeefcec851ce864d
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-08-19 21:54:55 +0530

    feat(db): add database connection module

---
+++ Backend/src/db/db.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+async function connectDB(){
+    try{
+
+        await mongoose.connect(process.env.MONGO_URI)
+
+        console.log("DB connected")
+
+    }
+    catch(err){
+
+        console.log("DB connected failed: ", err)
+
+    }
+}
+
+export default connectDB
+
