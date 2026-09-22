b795d7d92545046dd667e2c8ea6ab6e63401824f
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-09-22 14:34:42 +0530

    feat: add URL schema and model with click tracking

---
+++ server/src/models/urlmodel.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+
+const urlSchema = new mongoose.Schema({
+    originalUrl:{
+        type:String,
+        required: true
+    },
+    shortCode:{
+        type:String,
+        required: true,
+        unique: true
+    },
+    clicks:{
+        type:Number,
+        default:0
+    }
+},{
+    timestamps: true
+})
+
+const urlModel = mongoose.model('url', urlSchema)
+
+export default urlModel
