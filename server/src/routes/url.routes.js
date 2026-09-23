0faeae7264f01169a2a41cd6016b5538654b131e
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-09-23 20:08:30 +0530

    feat: add route to delete URL by id

--- server/src/routes/url.routes.js
+++ server/src/routes/url.routes.js
@@ -89,4 +89,30 @@ router.get('/:code', async function (req,res){
     })
 })
 
+
+/**
+ * @DELETE /api/url//:id
+ */
+
+router.delete("/:id", async function (req, res){
+
+    const {id} = req.params
+
+    const url = await urlModel.findById(id)
+
+    if(!url){
+        return res.status(404).json({
+            messgae:"URL not found"
+        })
+    }
+
+
+    await urlModel.findByIdAndDelete(id)
+
+    return res.status(200).json({
+        message:"URL deleted successfully"
+    })
+})
+
+
 export default router
